0176080e61089e44bb30120ab0804c6fa36dbcdc
DickTator97 <107931017+DickTator97@users.noreply.github.com>
2022-07-29 03:18:18 +0300

    Added Unity HomeWork  Mobile  Maze Game
    this is work in progress the game still needs to be made
    - to add: player prefs , json and serialization
    - to add :scriptable objects(If needed)
    - to Add: anything else in the brief on both advanced and mobile

---
+++ 2D Maze Mobile Game( Work In Progress) edited in 29.7.22/Assets/Scripts/ObjectDragger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDragger : MonoBehaviour
+{
+    Vector3 offset;
+
+    
+
+    private void OnMouseDown()
+    {
+        //calculate the offset between mouse click and transform
+        Vector3 mousedown_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousedown_position.z = transform.position.z;
+
+        offset = mousedown_position - transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 new_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        new_position.z = transform.position.z;
+        transform.position = new_position - offset;
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("on mouse up");
+    }
+}
