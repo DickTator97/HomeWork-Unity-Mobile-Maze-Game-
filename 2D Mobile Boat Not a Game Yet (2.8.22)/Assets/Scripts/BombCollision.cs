3bdbb4d3966e7765ef304dda5146a0d85eb3ca47
DickTator97 <107931017+DickTator97@users.noreply.github.com>
2022-08-03 02:51:09 +0300

    .

---
+++ 2D Mobile Boat Not a Game Yet (2.8.22)/Assets/Scripts/BombCollision.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class  BombCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ship-Player"));
+        {
+
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+            Debug.Log("Bomb Went Off ,Ship Sank");
+        }
+
+    }
+
+
+}
---
+++ 2D Mobile Boat Not a Game Yet (2.8.22)/Assets/Scripts/Explosion.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    private void OnDestroy()
+    {
+        //explosion sprite
+        //explosion sound
+        //repat for boat but bigger 
+
+
+    }
+}
